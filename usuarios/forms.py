1296f417c8939295830cb5dcbe27d31f35c9f760
Dione <padilhadione1@gmail.com>
2022-07-04 20:56:25 -0300

    fix login validator

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -5,7 +5,7 @@ from crispy_forms.helper import FormHelper
 from django.core.exceptions import ValidationError
 
 class UsuariosForm(UserCreationForm): # formulario para o cadastro de novos usuarios
-    email = forms.EmailField(max_length=100, required=False, help_text="Insira um email valido.")
+    email = forms.EmailField(max_length=100, required=False, help_text="Insira um email valido, por exemplo: exemplo@exemplo.com")
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('cadastro/', UsuarioCreate.as_view(), name='cadastro'),
-    path('', auth_views.LoginView.as_view(template_name="registration/login.html"), name='login'),
+    path('', auth_views.LoginView.as_view(), name='login'),
     path('sair/', auth_views.LogoutView.as_view(), name='logout'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html"), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"), name='password_reset_done'),
