03a52d693ea2a741f3fc2a269fcb987f88f8b0a7
Dione <padilhadione1@gmail.com>
2022-07-05 23:42:21 -0300

    fix details and more

--- carros/models.py
+++ carros/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.forms import Widget
 
 carros_choice = (
     ('Citroen', 'Citroen'),
@@ -45,8 +44,85 @@ motorizacao = (
     ('Não', 'Não'),
 )
 
+ano_fabricacao = (
+    ('Não sabe informar o ano de fabricação', 'Não sabe informar o ano de fabricação'),
+    ('2022', '2022'),
+    ('2021', '2021'),
+    ('2020', '2020'),
+    ('2019', '2019'),
+    ('2018', '2018'),
+    ('2017', '2017'),
+    ('2016', '2016'),
+    ('2015', '2015'),
+    ('2014', '2014'),
+    ('2013', '2013'),
+    ('2012', '2012'),
+    ('2011', '2011'),
+    ('2010', '2010'),
+    ('2009', '2009'),
+    ('2008', '2008'),
+    ('2007', '2007'),
+    ('2006', '2006'),
+    ('2005', '2005'),
+    ('2004', '2004'),
+    ('2003', '2003'),
+    ('2002', '2002'),
+    ('2001', '2001'),
+    ('2000', '2000'),
+    ('1999', '1999'),
+    ('1998', '1998'),
+    ('1997', '1997'),
+    ('1996', '1996'),
+    ('1995', '1995'),
+    ('1994', '1994'),
+    ('1993', '1993'),
+    ('1992', '1992'),
+    ('1991', '1991'),
+    ('1990', '1990'),
+)
+
+ano_modelo = (
+    ('Não sabe informar o ano do modelo', 'Não sabe informar o ano do modelo'),
+    ('2023', '2023'),
+    ('2022', '2022'),
+    ('2021', '2021'),
+    ('2020', '2020'),
+    ('2019', '2019'),
+    ('2018', '2018'),
+    ('2017', '2017'),
+    ('2016', '2016'),
+    ('2015', '2015'),
+    ('2014', '2014'),
+    ('2013', '2013'),
+    ('2012', '2012'),
+    ('2011', '2011'),
+    ('2010', '2010'),
+    ('2009', '2009'),
+    ('2008', '2008'),
+    ('2007', '2007'),
+    ('2006', '2006'),
+    ('2005', '2005'),
+    ('2004', '2004'),
+    ('2003', '2003'),
+    ('2002', '2002'),
+    ('2001', '2001'),
+    ('2000', '2000'),
+    ('1999', '1999'),
+    ('1998', '1998'),
+    ('1997', '1997'),
+    ('1996', '1996'),
+    ('1995', '1995'),
+    ('1994', '1994'),
+    ('1993', '1993'),
+    ('1992', '1992'),
+    ('1991', '1991'),
+    ('1990', '1990'),
+)
+
 class Carros(models.Model): # modelo para o CRUD de carros
     nome_do_carro = models.CharField(max_length=30, verbose_name='Modelo do Carro:')
+    ano_de_fabricacao = models.CharField(max_length=50, choices=ano_fabricacao, verbose_name='Ano de Fabricação:', default='Não sabe informar o ano de fabricação')
+    ano_do_modelo = models.CharField(max_length=50, choices=ano_modelo, verbose_name='Ano do Modelo:', default='Não sabe informar o ano do modelo')
     marcas_de_carros = models.CharField(max_length=30, choices=carros_choice, verbose_name='Fabricante:', default='Outros')
     valor_do_carro = models.CharField(max_length=50, choices=valor_carros, verbose_name='Valor do Carro:', default='Não sabe informar')
     modelos_de_carros = models.CharField(max_length=30, choices=motor_carros, verbose_name='Motor:', default='1.0')
--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,21 +1,20 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from crispy_forms.helper import FormHelper
 from django.core.exceptions import ValidationError
 
-class UsuariosForm(UserCreationForm): # formulario para o cadastro de novos usuarios
+class UsuariosForm(UserCreationForm):# formulario para o cadastro de novos usuarios
+    username = forms.CharField(min_length=3, required=True, label='Usuário', 
+            help_text='Obrigatório 3 caracteres ou mais. Letras, números e @/./+/-/_ apenas.')
+    
     email = forms.EmailField(max_length=100, required=False, help_text="Insira um email valido, por exemplo: exemplo@exemplo.com")
+    
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
     
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper(self)    
-    
     def clean_username(self):
         u = self.cleaned_data['username']
         if User.objects.filter(username=u).exists():
-            raise ValidationError("O usuario {} já existe.".format(u))
+            raise ValidationError("O usuário {} já existe.".format(u))
         return u
