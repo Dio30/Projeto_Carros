a501f40aae38cd0871e134dc65ceb5c7791ed5c1
Dione <padilhadione1@gmail.com>
2022-07-19 17:46:14 -0300

    add validator error in username in login

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
 
 class UsuariosForm(UserCreationForm):# formulario para o cadastro de novos usuarios
     username = forms.CharField(min_length=3, required=True, label='Usuário')
@@ -22,3 +24,31 @@ class UsuariosForm(UserCreationForm):# formulario para o cadastro de novos usuar
     #    if User.objects.filter(email=e).exists():
     #        raise ValidationError("O email {} já existe.".format(e))
     #    return e
+    
+class UsuariosLoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=255, required=True)
+    password = forms.CharField(widget=forms.PasswordInput, required=True)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+    
+    def clean(self, *args, **kwargs):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        if username and password:
+            if not user:
+                raise forms.ValidationError('''O usuário {} não existe, 
+                    lembrando que o login não diferencia letras maiuscúlas das minusculas'''.format(username))
+            if not user.check_password(raw_password=password):
+                raise forms.ValidationError('A senha está incorreta')
+            if not user.is_active:
+                raise forms.ValidationError('O usuário {} não está ativo.'.format(username))
+        return super(UsuariosLoginForm, self).clean(*args, **kwargs)
+    
+    def login(self, request):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        return user
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,17 @@
 from django.urls import reverse_lazy
-from .forms import UsuariosForm
+from .forms import UsuariosForm, UsuariosLoginForm
 from django.views.generic.edit import CreateView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.views import LoginView
 
 class UsuarioCreate(SuccessMessageMixin, CreateView):
     template_name = 'register.html'
     form_class = UsuariosForm
     success_message = "Usuário criado com sucesso"
     success_url = reverse_lazy('login')
+    
+class UsuarioLogIn(LoginView):
+    form_class = UsuariosLoginForm
+    template_name = 'registration/login.html'
+    success_url = reverse_lazy('lista')
+    redirect_field_name = 'next'
