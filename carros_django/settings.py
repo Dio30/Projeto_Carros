c16f5662c0b8aaf5c14bfe542220d32363a5d4f5
Dione <padilhadione1@gmail.com>
2022-10-05 23:11:17 -0300

    fix static3

--- carros_django/settings.py
+++ carros_django/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.postgres',
     
     # meus apps
-    'static',
     'carros',
     'usuarios',
+    'static',
     'storages',
     'crispy_forms',
     'bootstrapform',
@@ -126,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static" or "staticfiles")
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [ BASE_DIR / "static", ]
 
 #Arquivos de midia
