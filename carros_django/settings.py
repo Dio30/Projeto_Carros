1c648bebf58ae240961ab68ac6224d6dd262b758
Dione <padilhadione1@gmail.com>
2022-10-05 23:06:07 -0300

    fix static3

--- carros_django/settings.py
+++ carros_django/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static", "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static" or "staticfiles")
 STATICFILES_DIRS = [ BASE_DIR / "static", ]
 
 #Arquivos de midia
